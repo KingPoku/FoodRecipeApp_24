d4175668430664b364e79d4603e49ec3d0ed0b45
Sibofranc <arlonfranc@gmail.com>
2024-04-22 17:36:33 +0200

    New folder with classes

---
+++ Functionalities/Users.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodRecipeApp_24.Functionalities
+{
+    public class Users
+    {
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; private set; }
+
+        public Users(string name, string email, string password) 
+        { 
+            Name = name;
+            Email = email;
+            Password = password;
+        }
+
+        public void ViewRecipe()
+        {
+
+        }
+
+    }
+}
